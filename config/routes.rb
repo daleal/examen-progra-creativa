9e0d71ff26fd5b9ee5f04ce564536cc380b7299b
Daniel Leal <dlleal@uc.cl>
2019-12-15 20:23:04 -0300

    Nested users and projects

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users do
+    resources :projects
+  end
+
+  get 'projects/index', to: 'projects#index'
+
+  root 'projects#index'
 end
