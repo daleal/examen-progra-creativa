c7e8b765fe42cd86fe1112586536ce405e66583f
Daniel Leal <dlleal@uc.cl>
2019-12-15 20:24:15 -0300

    Started adding the visual structure

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,28 @@
 class ProjectsController < ApplicationController
-  before_action :authenticate_user!
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def project_params
+    params.require(:project).permit(:title, :code, :user_id)
+  end
 end
